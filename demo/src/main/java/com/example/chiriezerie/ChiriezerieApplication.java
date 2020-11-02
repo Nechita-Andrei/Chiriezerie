46a9dcf23cef15862b0ebb710331b8978e630846
Nechita-Andrei <62114344+Nechita-Andrei@users.noreply.github.com>
2020-11-02 09:32:37 +0200

    Add files via upload

---
+++ demo/src/main/java/com/example/chiriezerie/ChiriezerieApplication.java
@@ -0,0 +1,13 @@
+package com.example.chiriezerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiriezerieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiriezerieApplication.class, args);
+	}
+
+}
