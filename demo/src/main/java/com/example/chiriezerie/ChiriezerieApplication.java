cbd808f6ccb841a9aae056168310280ec32781aa
radustavila <stavila.radu@yahoo.com>
2020-11-03 11:40:57 +0200

    database setup

--- demo/src/main/java/com/example/chiriezerie/ChiriezerieApplication.java
+++ demo/src/main/java/com/example/chiriezerie/ChiriezerieApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChiriezerieApplication {
 
 	public static void main(String[] args) {
+		System.out.println("[Initializing SpringApp]");
 		SpringApplication.run(ChiriezerieApplication.class, args);
+		System.out.println("[SpringApp Initialized]");
 	}
 
 }
